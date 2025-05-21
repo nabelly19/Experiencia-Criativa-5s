3457f0423b0b6f37fcd502dfd87ad664ad8efa9a
anabelly <anabellymontibeller@gmail.com>
2025-05-21 17:22:19 -0300

    Add: including chatCard

---
+++ frontend/exp_project/src/components/ChatCardComponent/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from "./styles.module.css"
+
+import brainIcon from "../../assets/HeadIcon.svg"
+import leftbubble from "../../assets/WhatsappMessage1.svg"
+import rightbubble from "../../assets/WhatsappMessage2.svg"
+import leftbubble2 from "../../assets/WhatsappMessage3.svg"
+
+
+export default function ChatCard() {
+    return (
+      <div className={styles.chatWrapper}>
+        <div className={styles.chatCard}>
+              <div className={`${styles.bubble} ${styles.left}`}>
+                <img src={leftbubble} className={styles.svg} alt="Bubble" />
+              </div>
+  
+              <div className={`${styles.bubble} ${styles.right}`}>
+                <img src={rightbubble} className={styles.svg} alt="Bubble" />
+              </div>
+  
+              <div className={`${styles.bubble} ${styles.left}`}>
+                <img src={leftbubble2} className={styles.svg} alt="Bubble" />
+              </div>
+  
+            </div>
+          </div>
+    );
+  }
--- frontend/exp_project/src/pages/HomePage/index.js
+++ frontend/exp_project/src/pages/HomePage/index.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import styles from "./styles.module.css"
+import React from "react";
+import styles from "./styles.module.css";
 
 // components
-import WaveBackground from '../../components/WaveComponent';
-import IntroCard from '../../components/IntroCardComponent';
-import Footer from '../../components/FooterComponent';
+import WaveBackground from "../../components/WaveComponent";
+import IntroCard from "../../components/IntroCardComponent";
+import Footer from "../../components/FooterComponent";
+import ChatCard from "../../components/ChatCardComponent";
 
 export default function HomePage() {
-    return (
-        <>
-            <main>
-                <section className={styles.section1}>
-                    <div className={styles.cardContainer}>
-                        <IntroCard />
-                    </div>
-                    <div className={styles.waveContainer}>
-                        <WaveBackground />
-                    </div>
-                </section>
+  return (
+    <>
+      <main className={styles.mainWrapper}>
 
-                <section className={styles.section2}>
-                    {/* Conteúdo aqui */}
-                </section>
-            </main>
+        <section className={styles.section1}>
+          <div className={styles.cardContainer}>
+            <IntroCard />
+          </div>
+        </section>
 
-            <WaveBackground />
-            <Footer />
+        <section className={styles.section2}>
+          <ChatCard className={styles.chatCardContainer}/>
+        </section>
 
-        </>
-    );
+        {/* <div className={styles.waveContainer}>
+          <WaveBackground />
+        </div> */}
+      </main>
+      <div className={styles.secondArea}>
+        <WaveBackground />
+      </div>
+      <Footer />
+    </>
+  );
 }
